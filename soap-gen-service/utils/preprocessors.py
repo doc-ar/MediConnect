a4ac68586e34e13897efe767d81122f265605614
docar <weused@gmail.com>
2024-11-14 07:29:01 +0500

    removed everything to fix conflicts

--- soap-gen-service/utils/preprocessors.py
+++
@@ -1,8 +0,0 @@
-import re
-
-
-# Function to remove \n and punctuations from input text
-def preprocess_text(text):
-    text = re.sub(r'\n', ' ', text)
-    text = re.sub(r'[^\w\s]', '', text)
-    return text
